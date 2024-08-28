8328159350513ccfc731bd0c4d54809183056e72
Shehrozkhannn <sharozekkhan@gmail.com>
2024-08-29 01:54:12 +0500

    update changes

--- src/app/main-slider-img/main-slider-img.component.ts
+++ src/app/main-slider-img/main-slider-img.component.ts
@@ -26,12 +26,12 @@ slides = [
   {
     description: 'REAL BEAT TRX',
     backgroundColor: 'linear-gradient(90deg, rgba(177,205,2,1) 46%, rgba(13,224,185,1) 100%)',
-    image: '../../assets/518-5188983_music-icons-png-free-transparent-png-removebg-preview.png'
+    image: 'assets/518-5188983_music-icons-png-free-transparent-png-removebg-preview.png'
   },
   {
     description: 'SOLO BEAT FRX',
     backgroundColor: 'linear-gradient(90deg, rgba(13,95,224,1) 38%, rgba(177,205,2,1) 100%)',
-    image: '../../assets/string.png',
+    image: 'assets/string.png',
   }
 ];
 slideConfig = {
