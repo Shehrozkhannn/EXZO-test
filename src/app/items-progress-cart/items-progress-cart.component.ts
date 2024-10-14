0609d1e41968468e58ec4541b90c7507a9796d34
Shehrozkhannn <sharozekkhan@gmail.com>
2024-10-15 04:06:09 +0500

    Show loader and add price for added cart items

--- src/app/items-progress-cart/items-progress-cart.component.ts
+++ src/app/items-progress-cart/items-progress-cart.component.ts
@@ -3,28 +3,34 @@ import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose } from '@angular/material/dialog';
 import { DataService } from '../data.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-items-progress-cart',
   standalone: true,
-  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule,CommonModule],
+  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule,CommonModule,MatProgressSpinnerModule],
   templateUrl: './items-progress-cart.component.html',
   styleUrl: './items-progress-cart.component.scss'
 })
 export class ItemsProgressCartComponent implements OnInit {
   cartItems: any = [];
   totalCartPrice:number = 0;
+  loading:boolean = false;
  
-
-  constructor(public dataService: DataService){
-
-  }
+  constructor(public dataService: DataService, ){}
 
   async ngOnInit(){
-    const {cartItems }= await this.dataService.addToCartData();
-    this.cartItems = cartItems;
-    this.totalCartPrice = this.cartItems.reduce((acc: any, val: { price: any; })=> acc + Number(val.price) ,0)
-    console.log(this.totalCartPrice);
-    
+    try{
+      this.loading = true;
+      const {cartItems }= await this.dataService.addToCartData();
+      this.cartItems = cartItems;
+      this.totalCartPrice = this.cartItems.reduce((acc: any, val: { price: any; })=> acc + Number(val.price) ,0)    
+    }
+    catch(error){
+      console.error('Error fetching cart data:', error);
+    }
+    finally{
+      this.loading =  false;
+    }
   }
 }
